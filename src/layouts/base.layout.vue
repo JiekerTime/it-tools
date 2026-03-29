a0ac346fc346536e84e94cedcbe88293e007ece7
JiekerTime <zhangjunjie@apache.org>
2026-03-29 17:36:37 +0800

    feat: replace sponsor button with Alipay QR code popover
    - Remove buymeacoffee support button
    - Add Alipay QR code shown via n-popover on click
    - Remove sidebar footer author link
    - Add alipay-qr.jpg to public assets

--- src/layouts/base.layout.vue
+++ src/layouts/base.layout.vue
@@ -2,7 +2,7 @@
 import { NIcon, useThemeVars } from 'naive-ui';
 
 import { RouterLink } from 'vue-router';
-import { Heart, Home2, Menu2 } from '@vicons/tabler';
+import { Home2, Menu2 } from '@vicons/tabler';
 
 import { storeToRefs } from 'pinia';
 import HeroGradient from '../assets/hero-gradient.svg?component';
@@ -12,7 +12,6 @@ import { useStyleStore } from '@/stores/style.store';
 import { config } from '@/config';
 import type { ToolCategory } from '@/tools/tools.types';
 import { useToolStore } from '@/tools/tools.store';
-import { useTracker } from '@/modules/tracker/tracker.services';
 import CollapsibleToolMenu from '@/components/CollapsibleToolMenu.vue';
 
 const themeVars = useThemeVars();
@@ -20,7 +19,6 @@ const styleStore = useStyleStore();
 const version = config.app.version;
 const commitSha = config.app.lastCommitSha.slice(0, 7);
 
-const { tracker } = useTracker();
 const { t } = useI18n();
 
 const toolStore = useToolStore();
@@ -79,12 +77,6 @@ const tools = computed<ToolCategory[]>(() => [
               </c-link>
             </template>
           </div>
-          <div>
-            © {{ new Date().getFullYear() }}
-            <c-link target="_blank" rel="noopener" href="https://corentin.tech?utm_source=it-tools&utm_medium=footer">
-              Corentin Thomasset
-            </c-link>
-          </div>
         </div>
       </div>
     </template>
@@ -120,20 +112,17 @@ const tools = computed<ToolCategory[]>(() => [
           <NavbarButtons v-if="!styleStore.isSmallScreen" />
         </div>
 
-        <c-tooltip position="bottom" :tooltip="$t('home.support')">
-          <c-button
-            round
-            href="https://www.buymeacoffee.com/cthmsst"
-            rel="noopener"
-            target="_blank"
-            class="support-button"
-            :bordered="false"
-            @click="() => tracker.trackEvent({ eventName: 'Support button clicked' })"
-          >
-            {{ $t('home.buyMeACoffee') }}
-            <NIcon v-if="!styleStore.isSmallScreen" :component="Heart" ml-2 />
-          </c-button>
-        </c-tooltip>
+        <n-popover trigger="click" placement="bottom">
+          <template #trigger>
+            <c-button round class="support-button" :bordered="false">
+              打赏支持
+            </c-button>
+          </template>
+          <div class="alipay-qr-wrapper">
+            <img src="/alipay-qr.jpg" alt="Alipay QR Code" class="alipay-qr-image" />
+            <p class="alipay-qr-tip">支付宝扫码支持</p>
+          </div>
+        </n-popover>
       </div>
       <slot />
     </template>
@@ -141,27 +130,30 @@ const tools = computed<ToolCategory[]>(() => [
 </template>
 
 <style lang="less" scoped>
-// ::v-deep(.n-layout-scroll-container) {
-//     @percent: 4%;
-//     @position: 25px;
-//     @size: 50px;
-//     @color: #eeeeee25;
-//     background-image: radial-gradient(@color @percent, transparent @percent),
-//         radial-gradient(@color @percent, transparent @percent);
-//     background-position: 0 0, @position @position;
-//     background-size: @size @size;
-// }
-
 .support-button {
-  background: rgb(37, 99, 108);
-  background: linear-gradient(48deg, rgba(37, 99, 108, 1) 0%, rgba(59, 149, 111, 1) 60%, rgba(20, 160, 88, 1) 100%);
+  background: #1677ff;
   color: #fff !important;
-  transition: padding ease 0.2s !important;
+  transition: opacity ease 0.2s !important;
 
   &:hover {
-    color: #fff;
-    padding-left: 30px;
-    padding-right: 30px;
+    opacity: 0.85;
+  }
+}
+
+.alipay-qr-wrapper {
+  text-align: center;
+  padding: 8px;
+
+  .alipay-qr-image {
+    width: 220px;
+    display: block;
+    margin: 0 auto;
+  }
+
+  .alipay-qr-tip {
+    margin-top: 8px;
+    color: #888;
+    font-size: 12px;
   }
 }
 
